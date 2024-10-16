2bcb5baae231c77eb8f9b862e489258434d3c831
Nirmal Yadav <nirmal.yadav@laminaar.in>
2024-10-16 14:44:30 +0530

    med js practice

---
+++ src/mediumJS/searching.js
@@ -0,0 +1,27 @@
+export const search = (arr, ele)=>{
+
+ 
+    
+
+    let  right = arr.length-1
+    let left = 0
+
+    while(left <= right ){
+
+        let mid = Math.floor((right+left)/2)
+
+        if(arr[mid] === ele){
+            return ele
+        }
+        if(arr[mid] > ele){
+right= mid-1
+        }
+
+        if(arr[mid] < ele){
+            left = mid +1
+        }
+        
+    }
+
+    return "not found "
+}
